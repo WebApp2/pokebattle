10c3ce92fb60037a4cf41a2e8c6e3b153643f3c9
Rob <rnice01@aurora.edu>
2015-04-22 18:41:27 -0500

    Battle changes
    Working on player/AI turns

--- app/views/poke_battle/battleScreen.php
+++ app/views/poke_battle/battleScreen.php
@@ -14,11 +14,12 @@
 
 <?php
 
-$pokemon_id = 1;
+$p1_id = 1;
 
 
 $results1 = DB::select(DB::raw
-  ('SELECT image_url, name, health, attack, pokemon_type from pokemon where pokemon_id = 1'));
+  ('SELECT image_url, name, health, attack, pokemon_type from pokemon where pokemon_id = :p1_id'), array(
+   'p1_id' => $p1_id,));
 $list = array();
 foreach($results1 as $index => $val ){
   foreach($val as $i => $v){
@@ -38,77 +39,76 @@ foreach($list as $index => $val){
   $type = $val['pokemon_type'];
 }
 
-print $type;
- $attackName = DB::select(DB::raw('SELECT attack_name from moves where attack_type_pk = $type));
+$attackName =  DB::table('moves')->where('attack_type_pk', $type)->pluck('attack_name');
 
-
-       print"<div><img src= '$pic' style='width:150px;height:150px;position: relative' id='player1pic'/>
+       print "<div><img src= $pic style='width:150px;height:150px;position: relative' id='player1pic'/>
         <br/>Name: $name <br/>Health: $health<br/>Attack: $attack<br/><div>";
 
         //Attack button, used for jquery onclick function
-        print"<input type ='submit' id='p1attack' value=''>";
- 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pkmngame";
-$pokemon_id = 1;
+        print"<input type ='submit' id='p1attack' value='$attackName'>";
 
-$pokemon_2 =  rand( 1,  9);
- $connect = new mysqli($servername, $username,$password,$dbname)
-      or die ("Cannot connect to $server. Error: " . mysql_error());
-      //$name = DB::table('pokemon_images')->where('img_id', 1)->pluck('img_name');
 
-      $query = "SELECT image_url, name, health, attack FROM pokemon where pokemon_id = $pokemon_2 ";
-      $result = $connect->query($query);
 
-      if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $pic2 = $row["image_url"];
-        $health2= $row["health"];
-        $attack2= $row["attack"];
-        $name2 = $row["name"];
-        ?>
+?>
         
           </div>
         </div>
           <div class="container">
             <div style ='margin-left:250px'>
 
-        <?php
-        print"<div class = 'container' style='margin-left:250'><img src= '$pic2' style='width:150px;height:150px' /> 
+<?php
+//Get random pokemon ID
+$p2_id = rand( 1,  9);
+$results1 = DB::select(DB::raw
+  ('SELECT image_url, name, health, attack, pokemon_type from pokemon where pokemon_id = :p2_id'), array(
+   'p2_id' => $p2_id,));
+$list = array();
+foreach($results1 as $index => $val ){
+  foreach($val as $i => $v){
+    $list['pokemon'][$i] = $v;
+  }
+}
+$pic2 = '';
+$name2 = '';
+$health2 = '';
+$attack2 = '';
+$type2 = '';
+foreach($list as $index => $val){
+  $pic2 = $val['image_url'];
+  $name2 = $val['name'];
+  $health2 = $val['health'];
+  $attack2 = $val['attack'];
+  $type2 = $val['pokemon_type'];
+}
+
+$attackName2 =  DB::table('moves')->where('attack_type_pk', $type2)->pluck('attack_name');
+        print"<div class = 'container' style='margin-left:250'><img src= '$pic2' style='width:150px;height:150px;position: relative' id='player2pic' /> 
         <br/>Name: $name2 <br/>Health: $health2<br/>Attack: $attack2<br/></div>";
+        print"<div id='attackName2'></div>";
 
         ?>
 
       </body>
 
-        <?php
-       
-
-    }
-} else {
-    echo "0 results";
-}    
-
-
-
-?>
 <script>
   $(document).ready(function(){
       //Create variables from the PHP for both players' pokemon
+      
        var name = "<?php echo $name; ?>" ;
        var health = "<?php echo $health; ?>" ;
        var attack = "<?php echo $attack; ?>" ;
 
         var name2 = "<?php echo $name2; ?>" ;
        var health2 = "<?php echo $health2; ?>" ;
+       var newHealth1 = health;
        var newhealth2 = health2;
 
        var attack2 = "<?php echo $attack2; ?>" ;
-       //Set the health stat number on load
-       $("#p2healthstat").html(newhealth2);
+
+
+
+       //Set player 2 the health stat number on load
+       $("#p2healthstat").html(newhealth2 + "/" + health2);
         $(function() {
 
        //Sets current and max health of player 2's pokemon for progress bar
@@ -117,7 +117,9 @@ $pokemon_2 =  rand( 1,  9);
       });
       });
       
+     
     $('#p1attack').click(function(){//PLayer 1 attack button function
+    
         //Moves the player 1 picutre right 50px then resets
         $( "#player1pic" ).animate({
            left: "50px"
@@ -133,15 +135,20 @@ $pokemon_2 =  rand( 1,  9);
         window.location.replace("/victory");
         
        }
+
        
        //alert(name + " did " + attack + " damage to " + name2 +"!");
        //Applies damage to player 2 and updates progress bar value to damaged health.
        newhealth2 -= attack;
-       $("#p2healthstat").html(newhealth2);
+       $("#p2healthstat").html(newhealth2 + "/" + health2);
        $( "#p2healthbar" ).progressbar({
        value: parseInt(newhealth2)
       });
-    });
+
+
+
+});
+
   });
 
 
@@ -168,6 +175,8 @@ width:25%;
 <body>
  
 <div id="p2healthbar"></div><p id="p2healthstat"></p>
+
+<div style ='position:relative;margin-right:50px'id="p1healthbar"></div><p id="p1healthstat"></p>
  
  
 </body>
