c78d8fad6d508535ce3c8d9a158b69bfe809d6b4
Rob <rnice01@aurora.edu>
2015-04-25 16:21:24 -0500

    Fixed navbar links

--- app/views/poke_battle/battleScreen.php
+++ app/views/poke_battle/battleScreen.php
@@ -32,7 +32,7 @@ html, body, .container {
 }
 </style>
 </head>
-<header>
+      <header>
    <div class="navbar-wrapper">
       <div class="container">
 
@@ -49,21 +49,11 @@ html, body, .container {
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
+                <li><a href="#">Home</a></li>
+               <li><a href="players">Top Players</a></li>
+                <li><a href="pokemon">Top Pokemon</a></li>
+                <li class="active"><a href="battle">Battle!</a></li>
+               
               </ul>
             </div>
           </div>
