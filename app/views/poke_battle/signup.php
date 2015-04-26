0589b6261a7d5cba8dca513dfbbc42da229ce0f2
Brittany <WebDev2>
2015-04-25 19:59:47 -0500

    Database is in /app folder

--- app/views/poke_battle/signup.php
+++ app/views/poke_battle/signup.php
@@ -71,14 +71,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT name, image_url FROM pokemon";
+$sql = "SELECT name, pokemon_id, image_url FROM pokemon";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo '
-        <td> <input type="checkbox"> <b>
+        <td> <input type="checkbox" value="
+        ' 
+        . $row['pokemon_id'] . '
+        "><b>
         ' 
         . $row['name'] .  
         '
